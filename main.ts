0cbbf4be1084e213df4a05027a4c1138bdc54caf
HelloRocket <justin@hellorocket.org>
2020-10-24 21:50:16 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    Direction2 = Directions_List[List_Item]
+    basic.showString(Direction2)
+    List_Item += 1
+})
+input.onButtonPressed(Button.B, function () {
+    if (Direction2 != "M") {
+        Route.push(Direction2)
+    } else if (Direction2 == "M") {
+        for (let value of Route) {
+            basic.showString("" + (value))
+        }
+    }
+    List_Item = 0
+    basic.showIcon(IconNames.Yes)
+})
+let Direction2 = ""
+let Route: string[] = []
+let Directions_List: string[] = []
+let List_Item = 0
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    # . # . .
+    . # . . .
+    `)
+basic.clearScreen()
+List_Item = 0
+Directions_List = ["a", "b", "c"]
+Route = []
+Direction2 = Directions_List[List_Item]
+basic.showString(Direction2)
+List_Item += 1
